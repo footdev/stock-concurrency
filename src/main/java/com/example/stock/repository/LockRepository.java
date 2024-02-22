9a5084d5f90b235aeb5002841d11e4cbac8b71ad
ki_mensoo <ekclstkfka5@gmail.com>
2024-02-23 03:04:47 +0900

    refactor: LockRepository 오탈자 수정

--- src/main/java/com/example/stock/repository/LockRepository.java
+++ src/main/java/com/example/stock/repository/LockRepository.java
@@ -9,6 +9,6 @@ public interface LockRepository extends JpaRepository<Stock, Long> {
     @Query(value = "select get_lock(:key, 3000)", nativeQuery = true)
     public void getLock(String key);
 
-    @Query(value = "select release_lock(:key, 3000)", nativeQuery = true)
+    @Query(value = "select release_lock(:key)", nativeQuery = true)
     public void releaseLock(String key);
 }
